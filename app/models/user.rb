f503fbe375f8612245fd5e87681d2652e8a85362
Sachin Shah <sachinshah1999@gmail.com>
2023-11-01 19:11:42 +0530

    add active_storage and sent image url in API response

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,12 @@
 class User < ApplicationRecord
 
 	has_secure_password
+	has_one_attached :avatar
 	has_many :posts, dependent: :destroy
 	has_many :notifications
 
 	validates :name, presence: true
-	validates :password, presence: true
+	validates :password, presence: true, on: :create
 	validates :email, presence: true
 	validates :username, presence: true, uniqueness: true
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,8 @@
 class UserSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :name, :email, :password, :username
+  attributes :name, :email, :password, :username, :avatar
+
+  attribute :avatar do |ob|
+     "http://127.0.0.1:3000/" + Rails.application.routes.url_helpers.rails_blob_url(ob.avatar, only_path: true)
+  end
 end
